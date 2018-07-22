f37c5d0e41325132764a7f2de605653dde545d8d
Indu Ramesh <induramesh@gmail.com>
2018-07-22 18:34:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
   if (cart[i].itemName === item) {
-    return cart.splice(i,1);
+    cart.splice(i,1);
+    return cart; 
   } else {
     return ("That item is not in your cart.");
   }
