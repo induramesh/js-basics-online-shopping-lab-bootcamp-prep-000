550e97156dd20707a7d1034c1dc597142df7c956
Indu Ramesh <induramesh@gmail.com>
2018-07-22 18:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,8 +54,7 @@ function placeOrder(cardNumber) {
   if (! cardNumber) {
     return ("Sorry, we don't have a credit card on file for you.");
   } else {
-    var message = (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    cart = [];
+    return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     return message;
   }
 
