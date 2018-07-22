e9e1f9b6bdf3844096b24b3e58fc9f7fc0247f19
Indu Ramesh <induramesh@gmail.com>
2018-07-22 18:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
   }
   if (cart.length === 0) {
     return "Your shopping cart is empty."
-  }
+   }
   else if (list.length === 1) {
     return (`In your cart, you have ${list.join(', ' )}.`);
   } else {
@@ -36,7 +36,7 @@ function total() {
     var sum = 0
     for (let i = 0; i < cart.length; i++) {
        sum += cart[i].itemPrice
-      }
+    }
   return sum
  }
 
@@ -45,13 +45,13 @@ function removeFromCart(item) {
   if (cart[i].itemName === item) {
     cart.splice(i,1);
     return cart; 
-  } 
+   } 
   }
   return ("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
-  if (!cardNumber) {
+  if (! cardNumber) {
     return ("Sorry, we don't have a credit card on file for you.");
   } else {
     var message = (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
